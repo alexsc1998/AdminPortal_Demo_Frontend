d23501cb2232b9e1572519cf6a54b627644314d0
timofei220 <timofeicemco220@gmail.com>
2024-01-29 10:47:37 -0500

    Update: layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Providers } from './providers';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 
 const inter = Inter({
-  subsets: ['latin'],
-  fallback: ['Helvetica Neue', 'Arial'],
+  subsets: ["latin"],
+  fallback: ["Helvetica Neue", "Arial"],
 });
 
 export const metadata: Metadata = {
-  title: 'User Management Portal',
-  description: 'A user management portal',
+  title: "AFFIN BANK Onboarding",
+  description: "AFFIN BANK Onboarding",
 };
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel='icon' type='image/png' href='/logo.png' />
+        <link rel="icon" type="image/png" href="/logo.png" />
       </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
